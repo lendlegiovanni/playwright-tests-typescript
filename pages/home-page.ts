0865c2682f6365a0f4852220787281e71eb543af
Lendle Giovanni Vidal <lendle.giovanni.vidal@towerswatson.com>
2025-07-16 13:37:50 +0800

    Added data driven tests

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -2,9 +2,11 @@ import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
     readonly searchBox: Locator
+    readonly navigationLink: Locator
 
     constructor(private page: Page) {
         this.searchBox = page.getByTestId('constructor-search-input').describe('Search Box');
+        this.navigationLink = page.locator('.nav-item').describe('Navigation Link');
     }
 
     async goto() {
@@ -15,6 +17,10 @@ export class HomePage {
     async searchItem(item: string) {
         await this.searchBox.click();
         await this.searchBox.pressSequentially(item);
+    }
+
+    async getNavigationLinks() : Promise<Array<string>> {
+        return await this.navigationLink.allInnerTexts();
     }
 
 }
---
+++ tests/home.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from "../fixtures/fixtures";
+
+test.afterEach(async ({ page }, testInfo) => {
+    if (testInfo.status != 'passed') {
+        const screenshot = await page.screenshot();
+        await testInfo.attach('screenshot', {
+        body: screenshot,
+        contentType: 'image/png',
+        });
+    }
+    page.close();
+});
+
+//Navigation links data
+[
+    { name: 'Artificial Christmas Trees' },
+    { name: 'Wreaths' },
+    { name: 'Garlands' },
+    { name: 'Greenery & Flowers' },
+    { name: 'Seasonal Décor' },
+    { name: 'Storage' },
+].forEach(({ name }) => {
+    test.describe('Navigation tests', () => {
+        test(`${name} navigation is displayed`, async ({
+            homePage
+        }) => {
+            //Act
+            await test.step('Go to Balsam Hill site', async () => {
+                await homePage.goto();
+            });
+            
+            //Assert
+            await test.step(`Verify if the ${name} navigation is displayed`, async () => {
+                const navigationLinks = await homePage.getNavigationLinks();
+                expect(navigationLinks).toContain(name.toLocaleUpperCase());
+            });
+        });
+
+    });
+});
